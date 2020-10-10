ee64ca67e0f65760785eb16bb5270da642214eaf
Shoykot-Ahamed <shoykotahamed7736@gmail.com>
2020-10-10 07:22:55 +0600

    more 3

--- js/main.js
+++ js/main.js
@@ -174,7 +174,45 @@ $(document).ready(function() {
 	}
 	
   });
+  // team - active
+  $('.active-tools').slick({
+	dots: false,
+	autoplay: true,
+	autoplaySpeed: 10000,
+	arrows: true,
+	infinite: true,
+	prevArrow: '.arrow-prev',
+	nextArrow: '.arrow-next',
+	speed: 1000,
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	responsive: [
+	  {
+		breakpoint: 1024,
+		settings: {
+		  slidesToShow: 3,
+		  slidesToScroll: 1,
+		  infinite: true,
+		  dots: false
+		}
+	  },
+	  {
+		breakpoint: 991,
+		settings: {
+		  slidesToShow: 2,
+		  slidesToScroll: 1
+		}
+	  },
+	  {
+		breakpoint: 767,
+		settings: {
+		  slidesToShow: 1,
+		  slidesToScroll: 1
+		}
+	  }
 
+	]
+  });
 
 // WOW active
 new WOW().init();
